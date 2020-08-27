7e7cc4cfac4a8e3521dceba9722bbb4a216ce092
Hermann Kekeli GOLO <danielgolo82@gmail.com>
2020-08-27 21:38:11 +0000

    The last go!

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "19";
+	static const char DATE[] = "27";
 	static const char MONTH[] = "08";
 	static const char YEAR[] = "2020";
 	static const char UBUNTU_VERSION_STYLE[] =  "20.08";
@@ -15,18 +15,18 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR  = 1;
-	static const long MINOR  = 3;
-	static const long BUILD  = 390;
-	static const long REVISION  = 2044;
+	static const long MINOR  = 4;
+	static const long BUILD  = 424;
+	static const long REVISION  = 2278;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 527;
-	#define RC_FILEVERSION 1,3,390,2044
-	#define RC_FILEVERSION_STRING "1, 3, 390, 2044\0"
-	static const char FULLVERSION_STRING [] = "1.3.390.2044";
+	static const long BUILDS_COUNT  = 575;
+	#define RC_FILEVERSION 1,4,424,2278
+	#define RC_FILEVERSION_STRING "1, 4, 424, 2278\0"
+	static const char FULLVERSION_STRING [] = "1.4.424.2278";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 90;
+	static const long BUILD_HISTORY  = 24;
 	
 
 }
